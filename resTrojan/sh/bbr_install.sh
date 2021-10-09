0c2629438d41d74b9b0a855c1402f069e701483d
sunhoo911 <cloudyc86@gmail.com>
2021-10-10 00:57:31 +0800

    -

---
+++ resTrojan/sh/bbr_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#&& chmod +x trojan3.sh
+
+
+rm -f ./tcp.sh
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
+
+
+
---
+++ resTrojan/sh/check_trojan.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+APP_NAME=trojan
+
+
+ps aux |grep ${APP_NAME} 
+
+
