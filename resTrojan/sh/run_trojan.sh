ee545654079f3292e3cc6f7e97673741ff0e0e9e
sunhoo911 <cloudyc86@gmail.com>
2021-10-11 03:15:29 +0800

    修改

--- resTrojan/sh/run_trojan.sh
+++ resTrojan/sh/run_trojan.sh
@@ -4,21 +4,22 @@
 systemctl restart trojan
 
 ###############restart###############
-#APP_NAME=trojan 
-#APP_PATH=/usr/src/trojan/trojan 
-#APP_CONFIG=/usr/src/trojan/server.conf
+APP_NAME=trojan 
+APP_PATH=/usr/src/trojan/trojan 
+APP_CONFIG=/usr/src/trojan/server.conf
 
-#pid=`ps -ef | grep $APP_PATH | grep -v grep |awk '{print $2}'`
-#if [[ -n ${pid} ]]; then
-#    echo "App $APP_NAME is alreading running, pid=$pid"
-#else
-#    echo "App $APP_NAME is not running,now start..."
-#    /usr/src/trojan/trojan -c /usr/src/trojan/server.conf > /dev/null 2>&1 &
-#    sleep 2
-#    pidnew=`ps -ef | grep $APP_PATH | grep -v grep |awk '{print $2}'`
-#	if [[ -n ${pidnew} ]]; then
-#        echo "App $APP_NAME start succeed, pid=$pidnew"
-#    else
-#        echo "App $APP_NAME start failed"
-#    fi
-#fi
+sleep 1
+pid=`ps -ef | grep '/usr/src/trojan/trojan -c /usr/src/trojan/server.conf' | grep -v grep |awk '{print $2}'`
+if [[ -n ${pid} ]]; then
+    echo "App $APP_NAME is alreading running, pid=$pid"
+else
+    echo "App $APP_NAME is not running,now start..."
+    /usr/src/trojan/trojan -c /usr/src/trojan/server.conf > /dev/null 2>&1 &
+    sleep 2
+    pidnew=`ps -ef | grep '/usr/src/trojan/trojan -c /usr/src/trojan/server.conf' | grep -v grep |awk '{print $2}'`
+	if [[ -n ${pidnew} ]]; then
+        echo "App $APP_NAME start succeed, pid=$pidnew"
+    else
+        echo "App $APP_NAME start failed"
+    fi
+fi
--- resTrojan/sh/stop_trojan.sh
+++ resTrojan/sh/stop_trojan.sh
@@ -3,16 +3,15 @@
 #APP_NAME=trojan
 
 
-systemctl stop trojan
+#systemctl stop trojan
 
 
-#APP_NAME=trojan
-#pid=`ps -ef | grep $APP_NAME | grep -v grep |awk '{print $2}'`
-##if test $( pgrep -f $APP_NAME | wc -l ) -eq 0 ;then
-#if [[ -n ${pid} ]]; then
-#	echo "App $APP_NAME is running,pid=${pid},stop it..."
-#	kill -9 ${pid}
-#	echo App $APP_NAME stop succeed,pid=${pid}
-#else
-#	echo "App $APP_NAME is not running"
-#fi
+APP_NAME=trojan
+pid=`ps -ef | grep '/usr/src/trojan/trojan -c /usr/src/trojan/server.conf' | grep -v grep |awk '{print $2}'`
+if [[ -n ${pid} ]]; then
+	echo "App $APP_NAME is running,pid=${pid},stop it..."
+	kill -9 ${pid}
+	echo App $APP_NAME stop succeed,pid=${pid}
+else
+	echo "App $APP_NAME is not running"
+fi
