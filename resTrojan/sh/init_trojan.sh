e3ceda143faad5fd2830bb7bcb787438c8d6984b
sunhoo911 <cloudyc86@gmail.com>
2022-01-12 04:47:50 +0800

    Update init_trojan.sh

--- resTrojan/sh/init_trojan.sh
+++ resTrojan/sh/init_trojan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-
+cd ~/
 if [ ! -d "./trojan" ]; then
 mkdir -p ./trojan
 echo "创建成功trojan文件夹~"
@@ -9,24 +9,25 @@ else
 echo "trojan文件夹已存在鸭... "
 fi
 
+cd ~/trojan
 
 resTrojan_sh="https://raw.githubusercontent.com/sunhood911/filebank/main/resTrojan/sh/"
 
-echo "./trojan/stop_trojan.sh"
-rm -f ./trojan/stop_trojan.sh
-curl -o ./trojan/stop_trojan.sh ${resTrojan_sh}stop_trojan.sh && chmod +x ./trojan/stop_trojan.sh 
-echo "./trojan/trojan_install.sh"
-rm -f ./trojan/trojan_install.sh
-curl -o ./trojan/trojan_install.sh ${resTrojan_sh}trojan_install.sh && chmod +x ./trojan/trojan_install.sh  
-echo "./trojan/bbr_install.sh"
-rm -f ./trojan/bbr_install.sh
-curl -o ./trojan/bbr_install.sh ${resTrojan_sh}bbr_install.sh && chmod +x ./trojan/bbr_install.sh  
-echo "./trojan/run_trojan.sh"
-rm -f ./trojan/run_trojan.sh
-curl -o ./trojan/run_trojan.sh ${resTrojan_sh}run_trojan.sh && chmod +x ./trojan/run_trojan.sh 
-echo "./trojan/check_trojan.sh"
-rm -f ./trojan/check_trojan.sh
-curl -o ./trojan/check_trojan.sh ${resTrojan_sh}check_trojan.sh && chmod +x ./trojan/check_trojan.sh 
+echo " stop_trojan.sh"
+rm -f  stop_trojan.sh
+curl -o  stop_trojan.sh ${resTrojan_sh}stop_trojan.sh && chmod +x  stop_trojan.sh 
+echo " trojan_install.sh"
+rm -f  trojan_install.sh
+curl -o  trojan_install.sh ${resTrojan_sh}trojan_install.sh && chmod +x  trojan_install.sh  
+echo " bbr_install.sh"
+rm -f  bbr_install.sh
+curl -o  bbr_install.sh ${resTrojan_sh}bbr_install.sh && chmod +x  bbr_install.sh  
+echo " run_trojan.sh"
+rm -f  run_trojan.sh
+curl -o  run_trojan.sh ${resTrojan_sh}run_trojan.sh && chmod +x  run_trojan.sh 
+echo " check_trojan.sh"
+rm -f  check_trojan.sh
+curl -o  check_trojan.sh ${resTrojan_sh}check_trojan.sh && chmod +x  check_trojan.sh 
 
 
 
