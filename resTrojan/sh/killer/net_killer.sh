10ccebb192bfd45f74859b228ff5ec616e14f8d4
sunhoo911 <cloudyc86@gmail.com>
2023-03-16 00:16:23 +0800

    Update net_killer.sh

--- resTrojan/sh/killer/net_killer.sh
+++ resTrojan/sh/killer/net_killer.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 
-wget speedtest.py https://raw.github.com/sivel/speedtest-cli/master/speedtest.py && chmod +x speedtest.py
+wget speedtest.py https://raw.github.com/sivel/speedtest-cli/master/speedtest.py && chmod a+rx speedtest.py
 
 endless_loop()
 {
 do
 /usr/bin/python3 speedtest.py   1>log.log 2>&1 &
-sleep 300
+sleep 10
 done
 
 }
