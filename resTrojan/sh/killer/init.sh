a6fa963f01930131c689f7fbaeec31bd6003d18d
sunhoo911 <cloudyc86@gmail.com>
2023-03-15 23:51:53 +0800

    Update init.sh

--- resTrojan/sh/killer/init.sh
+++ resTrojan/sh/killer/init.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 
-if [ ! -d "./killer" ]; then
-mkdir -p ./killer
+dir=~/killer
+if [ ! -d "${dir}" ]; then
+mkdir -p ${dir}
 echo "创建成功killer文件夹~"
 else
 echo "killer文件夹已存在鸭... "
@@ -13,28 +14,28 @@ path="https://raw.githubusercontent.com/sunhood911/filebank/main/resTrojan/sh/ki
 
 echo " cpu_killer.sh"
 rm -f  cpu_killer.sh
-curl -o  ./killer/cpu_killer.sh ${path}cpu_killer.sh && chmod +x  cpu_killer.sh 
+curl -o  ${dir}/cpu_killer.sh ${path}cpu_killer.sh && chmod +x  cpu_killer.sh 
 echo " mem_killer.sh"
 rm -f  mem_killer.sh
-curl -o  ./killer/mem_killer.sh ${path}mem_killer.sh && chmod +x  mem_killer.sh  
+curl -o  ${dir}/mem_killer.sh ${path}mem_killer.sh && chmod +x  mem_killer.sh  
 echo " net_killer.sh"
 rm -f  net_killer.sh
-curl -o  ./killer/net_killer.sh ${path}net_killer.sh && chmod +x  net_killer.sh  
+curl -o  ${dir}/net_killer.sh ${path}net_killer.sh && chmod +x  net_killer.sh  
 
 echo " run_cpu_killer.sh"
 rm -f  run_cpu_killer.sh
-curl -o  ./killer/run_cpu_killer.sh ${path}run_cpu_killer.sh && chmod +x  run_cpu_killer.sh 
+curl -o  ${dir}/run_cpu_killer.sh ${path}run_cpu_killer.sh && chmod +x  run_cpu_killer.sh 
 echo " run_mem_killer.sh"
 rm -f  run_mem_killer.sh
-curl -o  ./killer/run_mem_killer.sh ${path}run_mem_killer.sh && chmod +x  run_mem_killer.sh 
+curl -o  ${dir}/run_mem_killer.sh ${path}run_mem_killer.sh && chmod +x  run_mem_killer.sh 
 echo " run_net_killer.sh"
 rm -f  run_net_killer.sh
-curl -o  ./killer/run_net_killer.sh ${path}run_net_killer.sh && chmod +x  run_net_killer.sh 
+curl -o  ${dir}/run_net_killer.sh ${path}run_net_killer.sh && chmod +x  run_net_killer.sh 
 
 
 echo " stop_all.sh"
 rm -f  stop_all.sh
-curl -o  ./killer/stop_all.sh ${path}stop_all.sh && chmod +x  stop_all.sh 
+curl -o  ${dir}/stop_all.sh ${path}stop_all.sh && chmod +x  stop_all.sh 
 
 
 
