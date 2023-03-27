6184e78749f9aa5507e530dabcf43b0b05555de6
sunhoo911 <cloudyc86@gmail.com>
2023-03-27 23:58:36 +0800

    Update net_killer.sh

--- resTrojan/sh/killer/net_killer.sh
+++ resTrojan/sh/killer/net_killer.sh
@@ -8,10 +8,13 @@ endless_loop()
 for((;;))
 do
 /usr/bin/python3 speedtest.py   1>log.log 2>&1 &
+rm -f speedtest.py.*
 sleep 180
 /usr/bin/python3 speedtest.py   1>log.log 2>&1 &
+rm -f speedtest.py.*
 sleep 300
 /usr/bin/python3 speedtest.py   1>log.log 2>&1 &
+rm -f speedtest.py.*
 sleep 240
 done
 
